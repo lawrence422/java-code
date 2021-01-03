6d3de570914aec3150a1ed1683a08e034bb12d88
lawrence422 <lawrence422@gmail.com>
2021-01-03 20:03:09 +0800

    branch4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         int a,b;
+        int j=4;
 	    System.out.println("my master branch.");
 	    System.out.println("branch1\n");
+		System.out.println("int:"+j);
 
 	    for (a=0;a<5;a++) {
 	        for(b=0;b<=a;b++){
