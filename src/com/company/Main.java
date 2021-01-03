5518c269db7e3e97f7a0da5db569e0498af40383
lawrence422 <lawrence422@gmail.com>
2021-01-03 20:28:25 +0800

    branch6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a,b;
+        int a,b,c;
         int j=4;
 	    System.out.println("my master branch.");
 	    System.out.println("branch1\n");
 		System.out.println("int:"+j);
-
-	    for (a=0;a<=10;a++) {
-	        if(a<5) {
-				for (b = 0; b <= a; b++) {
-					System.out.print("*");
+		for(c=0;c<3;c++) {
+			for (a = 0; a <= 10; a++) {
+				if (a < 5) {
+					for (b = 0; b <= a; b++) {
+						System.out.print("*");
+					}
+					System.out.print("\n");
+				} else {
+					for (b = 10; b >= a; b--) {
+						System.out.print("*");
+					}
+					System.out.print("\n");
 				}
-				System.out.print("\n");
 			}
-	        else {
-				for (b = 10; b >= a; b--) {
-					System.out.print("*");
-				}
-				System.out.print("\n");
-			}
-        }
+		}
     }
 }
