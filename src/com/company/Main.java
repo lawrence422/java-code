85d46bc26c52f7d479ab09cdcaa5ad5e7bf5fd99
lawrence422 <lawrence422@gmail.com>
2021-01-03 20:19:28 +0800

    branch5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,19 @@ public class Main {
 	    System.out.println("branch1\n");
 		System.out.println("int:"+j);
 
-	    for (a=0;a<5;a++) {
-	        for(b=0;b<=a;b++){
-	            System.out.print("*");
-            }
-	        System.out.print("\n");
+	    for (a=0;a<=10;a++) {
+	        if(a<5) {
+				for (b = 0; b <= a; b++) {
+					System.out.print("*");
+				}
+				System.out.print("\n");
+			}
+	        else {
+				for (b = 10; b >= a; b--) {
+					System.out.print("*");
+				}
+				System.out.print("\n");
+			}
         }
     }
 }
