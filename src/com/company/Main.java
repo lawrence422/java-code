5dd0a901dafe717f0dce396b6aeb90bdf47945ec
lawrence422 <lawrence422@gmail.com>
2021-01-03 19:50:30 +0800

    branch3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("my master branch.");
-	System.out.println("branch1\n");
+        int a,b;
+	    System.out.println("my master branch.");
+	    System.out.println("branch1\n");
+
+	    for (a=0;a<5;a++) {
+	        for(b=0;b<=a;b++){
+	            System.out.print("*");
+            }
+	        System.out.print("\n");
+        }
     }
 }
