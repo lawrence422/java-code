1e7461cf4ace65d81c2aa3b780d528612464d812
lawrence422 <lawrence422@gmail.com>
2021-01-03 20:42:45 +0800

    branch7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
         int a,b,c;
         int j=4;
+        double pi=3.14159;
 	    System.out.println("my master branch.");
 	    System.out.println("branch1\n");
 		System.out.println("int:"+j);
+		System.out.println("pi:"+pi);
 		for(c=0;c<3;c++) {
 			for (a = 0; a <= 10; a++) {
 				if (a < 5) {
