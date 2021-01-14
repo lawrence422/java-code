dc9504bf4176ec97ceb4f5dc97eeee1a7fc8646b
lawrence422 <lawrence422@gmail.com>
2021-01-14 15:17:42 +0800

    short circuit

--- src/com/company/digital.java
+++ src/com/company/digital.java
@@ -4,11 +4,24 @@ public class digital {
     public static void main(String [] argv){
         double a=1.1*3;
         double b=3.3*1;
-        Boolean c=false;
+        int i=4,j=3;
+        Boolean c=false,d=true;
         System.out.println("a="+a);
         System.out.println("b="+b);
         System.out.println("a=b\t"+(a==b));
         System.out.println("c !c=\t"+c+"\t"+!c);
 
+        System.out.println("c,d="+c+"\t"+d);
+        System.out.println("c&d="+(c&d));
+        System.out.println("c&&d="+(c&&d));
+        System.out.println("c|d="+(c|d));
+        System.out.println("c||d="+(c||d));
+        System.out.println("c^d="+(c^d)+"\n");
+        System.out.println("i="+i+"\tj="+j+(true|(i==j++)));
+        System.out.println("i="+i+"\tj="+j);
+        i=4;
+        j=3;
+        System.out.println("i="+i+"\tj="+j+(true||(i==j++)));
+        System.out.println("i="+i+"\tj="+j);
     }
 }
