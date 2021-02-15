cf4aba307dcd56b91aa556f9f255bf83937e7c22
lawrence422 <lawrence422@gmail.com>
2021-02-15 17:55:13 +0800

    module

---
+++ java_basic/src/java5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class java5 {
+    public static void main(String... argv) {
+        int input_number;
+        System.out.println("input");
+        Scanner sc = new Scanner(System.in);
+        input_number = sc.nextInt();
+        switch (input_number) {
+            case 10:
+                System.out.println("10");
+                break;
+
+            case 50:
+                System.out.println("50");
+                break;
+
+            default:
+                System.out.println("NONE");
+
+        }
+
+    }
+}
